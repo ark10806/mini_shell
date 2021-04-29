a0d1b72b562b230a9b694247ab21090d757493a7
ark10806@gmail.com <ark10806@gmail.com>
2021-04-30 06:14:16 +0900

    merged

---
+++ mini_shell.cpp
@@ -0,0 +1,15 @@
+//hi
+#include <iostream>
+using namespace std;
+
+string strs;
+
+int main(){
+	while(true){
+		cout << "12161161_shell$";
+		cin >> strs;
+		cout << strs << endl;
+	}
+
+	return 0;
+}
