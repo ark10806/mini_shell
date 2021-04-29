5ed6fa4c94e925d4410fe6dd98b2a8752468d3c3
ark10806@gmail.com <ark10806@gmail.com>
2021-04-30 06:52:33 +0900

    to VScode

--- mini_shell.cpp
+++ mini_shell.cpp
@@ -1,14 +1,29 @@
 //hi
 #include <iostream>
+#include <queue>
+#include <string>
 using namespace std;
 
-string strs;
+queue<string> oper_que;
+string command_line;
 
 int main(){
 	while(true){
 		cout << "12161161_shell$";
-		cin >> strs;
-		cout << strs << endl;
+		getline(cin, command_line);
+		cout << command_line;
+		
+		for(int i=0; i<command_line.size(); i++){
+			if(command_line[i]==' '){ 
+				oper_que.push(command_line.substr(0,i));
+			}
+		}
+
+		for(int i=0; i<oper_que.size(); i++){
+			//cout << '['  << oper_que.front() << ']' <<  endl;
+			oper_que.pop();
+			//cout << oper_que.pop() << endl;
+		}
 	}
 
 	return 0;
